b0351c6c9d7918dbf2a2c63e2a16edd3b376c0d4
jpchen <jpchen@lisystec.com>
2015-11-24 16:55:06 +0800

    update the config/env/development.js runonce_file.sh custom.sh  db config

--- onbuild/scripts/custom.sh
+++ onbuild/scripts/custom.sh
@@ -4,7 +4,7 @@ set -e
 
 # sync with linked database every restart
 if [[ -n "DB_PORT_27017_TCP_ADDR}" ]]; then
-    sudo -u node -H sed -ri "s|mongodb://.*|mongodb://${DB_PORT_27017_TCP_ADDR}/${FILE_DBNAME}',|g" ${APP_DIR}/config/env/${STACK_ENV}.js
+    sudo -u node -H sed -ri "s|mongodb://.*|mongodb://${DB_PORT_27017_TCP_ADDR}/${FILE_DBNAME}',|g" ${APP_DIR}/config/env/${FILE_ENV}.js
 
     if [[ -e ${APP_DIR}/config/env/local.js ]]; then
         sudo -u node -H sed -ri "s|mongodb://.*|mongodb://${DB_PORT_27017_TCP_ADDR}/${FILE_DBNAME}',|g" ${APP_DIR}/config/env/local.js
